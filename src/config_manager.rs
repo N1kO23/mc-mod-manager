5dbfc1f2a767a0809ca675fb05cdee6379f7c596
N1kO23 <niko.huuskonen.00@gmail.com>
2022-01-04 22:20:08 +0200

    Added config manager base

---
+++ src/config_manager.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::path::Path;
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Config {
+    pub minecraft_installation_path: String,
+    pub mod_download_path: String,
+}
+
+impl ConfigManager {
+    pub fn new() -> Result<ConfigManager> {
+        let config = ConfigManager::load_config()?;
+        return Ok(Self { config });
+    }
+
+    pub fn save_config(config: Config) -> Result<()> {
+        let file = File::create("config.json")?;
+        serde_json::to_writer(file, &config)?;
+        return Ok(());
+    }
+
+    /// Loads the config file from disk or creates a new one if it doesn't exist.
+    pub fn load_config() -> Result<Config> {
+        if !Path::new("config.json").exists() {
+            return Ok(ConfigManager::create_config()?);
+        } else {
+            let file = File::open("config.json")?;
+            return Ok(serde_json::from_reader(file)?);
+        }
+    }
+
+    /// Creates a new config file with an empty access token and default backend address.
+    pub fn create_config() -> Result<Config> {
+        let config = Config {
+            minecraft_installation_path: String::new(),
+            mod_download_path: String::new(),
+        };
+        ConfigManager::save_config(config.clone())?;
+        return Ok(config);
+    }
+}
