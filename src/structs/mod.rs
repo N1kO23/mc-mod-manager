497b187460fa33caef0f5f39a421c5de0c4a10c2
N1kO23 <niko.huuskonen.00@gmail.com>
2022-01-05 14:41:08 +0200

    Changed folder and file paths into pathbufs

--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,4 +1,13 @@
+use std::path::PathBuf;
+
 use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Config {
+    pub minecraft_installation_path: PathBuf,
+    pub mod_download_path: PathBuf,
+    pub active_prefix: Prefix,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Addon {
@@ -11,6 +20,7 @@ pub struct Addon {
     pub status: i32,
     pub gameId: i32,
     pub primaryCategoryId: i32,
+    pub downloadCount: f32,
     pub popularityScore: f32,
     pub gamePopularityRank: i32,
     pub dateCreated: String,
@@ -39,7 +49,9 @@ pub struct Prefix {
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Mod {
+    pub id: i32,
     pub name: String,
     pub version: String,
     pub file_name: String,
+    pub file_path: Option<PathBuf>,
 }
