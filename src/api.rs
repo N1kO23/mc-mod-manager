00588813138eccd762890a25d586bdfdc326b112
N1kO23 <niko.huuskonen.00@gmail.com>
2022-01-09 23:34:38 +0200

    initial implementation for download stream writer

--- src/api.rs
+++ src/api.rs
@@ -1,8 +1,7 @@
-use std::path::PathBuf;
-
-use anyhow::Result;
-
+extern crate reqwest;
 use crate::structs::{Addon, Mod};
+use anyhow::Result;
+use std::{fs::File, io, path::PathBuf};
 
 const CF_BASE_URL: &str = "https://addons-ecs.forgesvc.net/api/v2/addon/";
 
@@ -17,6 +16,16 @@ pub async fn search_addon(addon_id: i32) -> Result<Addon, anyhow::Error> {
 
 pub async fn download_addon(addon_id: i32, version: i32, download_path: PathBuf) -> Result<Mod> {
     // Todo: Implement function that downloads the addon from the curseforge server.
+    // Figure out the actual api endpoint for this.
+    let url = CF_BASE_URL;
+    let res_bytes = reqwest::get(url).await?.bytes().await?;
+    let mut file = File::create(format!(
+        "{}/{}-{}.jar",
+        download_path.to_string_lossy(),
+        addon_id,
+        version
+    ))?;
+    io::copy(&mut res_bytes.as_ref(), &mut file)?;
     let r#mod = Mod {
         id: addon_id,
         name: "".to_string(),
