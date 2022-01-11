46179744d25e29358f2d126eb586b60991136baa
N1kO23 <niko.huuskonen.00@gmail.com>
2022-01-11 16:18:31 +0200

    Added function for creating new prefixes

--- src/prefix_manager.rs
+++ src/prefix_manager.rs
@@ -1,5 +1,6 @@
 use crate::config_manager::ConfigManager;
 use crate::mod_manager::ModManager;
+use crate::util::read_line;
 use anyhow::Result;
 use std::fs::File;
 use std::path::Path;
@@ -54,7 +55,7 @@ impl PrefixManager {
 
     pub async fn add_mod_to_prefix(&mut self, id: i32, version: String) -> Result<()> {
         let mod_manager = ModManager::new()?;
-        let addon = mod_manager.get_mod(id, version)?;
+        let addon = mod_manager.get_mod(id, &version)?;
         if self.prefix.mod_list.contains(&addon.id) {
             return Err(anyhow::anyhow!("Mod already exists in prefix"));
         }
@@ -68,5 +69,36 @@ impl PrefixManager {
         // TODO: Implement function that removes the mod from the prefix and from the minecraft mods folder if this is the active prefix
         PrefixManager::save_prefix(self.prefix.clone())?;
         return Ok(());
+    }
+
+    pub fn new_prefix_form(name: &str) -> Result<Prefix> {
+        print!("{}", "Enter the author of the prefix (default: 'author'): ");
+        let author = read_line().unwrap_or("author".to_string());
+        print!("{}", "Enter the description of the prefix (default: ''): ");
+        let description = read_line().unwrap_or("".to_string());
+        print!("{}", "Enter the version of the prefix (default: '0.0.1'): ");
+        let version = read_line().unwrap_or("0.0.1".to_string());
+        print!(
+            "{}",
+            "Enter the game version of the prefix (default: '1.18.1'): "
+        );
+        let game_version = read_line().unwrap_or("1.18.1".to_string());
+        print!(
+            "{}",
+            "Enter the mod loader of the prefix (default: 'Forge'): "
+        );
+        let mod_loader = read_line().unwrap_or("Forge".to_string());
+        let prefix = Prefix {
+            name: name.to_string(),
+            description,
+            author,
+            version,
+            game_version,
+            mod_list: Vec::new(),
+            mod_loader,
+        };
+        println!("{}", "Prefix created!");
+        PrefixManager::save_prefix(prefix.clone())?;
+        return Ok(prefix);
     }
 }
